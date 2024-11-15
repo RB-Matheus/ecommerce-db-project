ed95dad6a1ac2e7c0e4eaf1032de3b4c2e363353
RB-Matheus <typematt4101@gmail.com>
2024-11-15 13:09:29 -0300

    Implementing stock movement audit at product registration

--- scripts/triggers.sql
+++ scripts/triggers.sql
@@ -36,3 +36,20 @@ begin
     end if;
 end $$
 delimiter ;
+
+-- audita movimentações de cadastro de produtos
+delimiter $$
+create trigger auditar_cadastro_produto
+after insert on produtos
+for each row
+begin
+
+	if validar_cadastro_produto(new.id_categoria, new.preco, new.quantidade) THEN
+		insert into movimentacoes_estoque (id_produto, preco_antes, preco_depois, qtd_antes, qtd_depois, data_movimentacao) values 
+		(new.id_produto, 0.00, new.preco, 0, new.quantidade, current_timestamp());
+    else 
+        signal sqlstate '45000' set message_text = 'Produto com especificações inválidas. Certifique-se de revisar os campos informados.';
+    end if;
+    
+end $$
+delimiter ;
