bb88da537b718e59b8fc9f337a77fc8ee62ea119
RB-Matheus <typematt4101@gmail.com>
2024-11-15 21:01:54 -0300

    Fixing cyclic reference between costumers and wishlists; implementing open tickets view

--- scripts/views.sql
+++ scripts/views.sql
@@ -7,4 +7,18 @@ select concat(c.nome+' ',c.sobrenome) as cliente, pr.nome as produto,quantidade,
     where month(p.data_pedido) = month(curdate());
 
 
-select * from pedidos_mes;
+-- select * from pedidos_mes;
+
+create view vw_tickets_abertos as
+select
+    t.id_ticket,
+    t.assunto,
+    t.descricao,
+    c.nome as cliente_nome,
+    c.sobrenome as cliente_sobrenome,
+    f.nome as funcionario_nome,
+    f.sobrenome as funcionario_sobrenome
+from tickets t
+join clientes c on t.id_cliente = c.id_cliente
+join funcionarios f on t.id_funcionario = f.id_funcionario
+where t.status_ticket = 'aberto';
