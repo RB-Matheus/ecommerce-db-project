9c2bb5ea843b704de2c6e5fcdfb97410ccd4e6b8
unknown <edilsonemidio93@gmail.com>
2024-11-15 22:26:33 -0300

    +2 views

--- scripts/views.sql
+++ scripts/views.sql
@@ -6,7 +6,6 @@ select concat(c.nome+' ',c.sobrenome) as cliente, pr.nome as produto,quantidade,
     join produtos pr on pr.id_produto = ip.id_produto
     where month(p.data_pedido) = month(curdate());
 
-
 -- select * from pedidos_mes;
 
 create view vw_tickets_abertos as
@@ -22,3 +21,22 @@ from tickets t
 join clientes c on t.id_cliente = c.id_cliente
 join funcionarios f on t.id_funcionario = f.id_funcionario
 where t.status_ticket = 'aberto';
+
+
+create or replace view compradores_mes as
+select sum(ip.quantidade * p.preco) as total, c.nome as cliente
+	from itens_pedido ip
+    join pedidos pe on pe.id_pedido = ip.id_pedido
+	join produtos p on p.id_produto = ip.id_produto
+    join clientes c on c.id_cliente = pe.id_cliente
+    group by pe.id_pedido
+    order by total desc 
+    limit 10;
+-- select * from compradores_mes;
+
+create or replace view produtos_promocao as
+select nome, (preco - desconto) as preco_final, data_fim
+	from produtos p
+    join promocoes d on p.id_produto = p.id_produto;
+    
+select * from produtos_promocao;
